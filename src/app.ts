c8b4494e5c4b7e3114822350bf4ccce5416ddbdf
nilesterci <nilmar67@gmail.com>
2023-10-29 11:50:09 -0300

    new cors

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:8100", "capacitor://localhost"],
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
   })
 );
